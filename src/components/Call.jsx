873a0d2d62e69f79a1ecb5b097bb5f202ba10f65
Jiehao Luo <powerseed5044@gmail.com>
2024-07-09 14:06:27 -0600

    Tell users it is operating when archiving or unarchiving a call in Call component.

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -1,5 +1,5 @@
 import Moment from "react-moment";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Call = (props) => {
     const dividerStyle = {
@@ -11,13 +11,13 @@ const Call = (props) => {
         backgroundRepeat: "repeat-x"
     };
 
-    let [isArchiveButtonDisplayed, setIsArchiveButtonDisplayed] = useState(false);
+    let [isArchiving, setIsArchiving] = useState(false);
 
-    function toggleArchiveButtonDisplay() {
-        setIsArchiveButtonDisplayed(!isArchiveButtonDisplayed);
-    }
+    useEffect(() => {
+        if (!isArchiving) {
+            return;
+        }
 
-    function archiveCall() {
         fetch(process.env.REACT_APP_BASE_URL_OF_API + "/activities/" + props.call.id, {
             method: 'PATCH',
             headers: {
@@ -35,7 +35,12 @@ const Call = (props) => {
                     throw new Error()
                 }
             })
+            .then(() => setIsArchiving(false))
             .catch((error) => { console.log(error) })
+    }, [isArchiving])
+
+    function archiveCall() {
+        setIsArchiving(true);
     }
 
     return (
@@ -48,54 +53,62 @@ const Call = (props) => {
                 <div style={dividerStyle}></div>
             </div>
 
-            <div className="w-[90%] flex items-center rounded-xl py-[15px] border" onClick={toggleArchiveButtonDisplay}>
-                <div className="mx-[20px]">
-                    {
-                        props.call.direction === "inbound" ?
-                            <img src="../../public/images/inbound-call.svg" title="Inbound" alt="Inbound" width={20} height={20} />
-                            :
-                            <img src="../../public/images/outbound-call.svg" title="Outbound" alt="Outbound" width={20} height={20} />
-                    }
-                </div>
-
-                <div className="grow flex flex-col space-y-[5px]">
-                    <div className="flex space-x-[5px]">
-                        <div className="text-[16px] font-bold text-[var(--text-color-primary)]"
-                            title={props.call.direction === "inbound" ? 'From' : 'To'}>
-                            {
-                                props.call.direction === "inbound" ? props.call.from : props.call.to
-                            }
-                        </div>
-
-                        <div className="w-[16px] h-[16px] flex justify-center items-center bg-red-600 text-white font-bold rounded-full"
-                            title="Via number">
-                            {props.call.via}
-                        </div>
-                    </div>
-
-                    <div className="text-[16px] text-[var(--text-color-secondary)]">
-                        {
-                            props.call.call_type === "answered" ?
-                                props.call.duration + " second" + (props.call.duration == 1 ? "" : "s") :
-                                props.call.call_type}
-                    </div>
-                </div>
-
-                <div className="flex items-center font-bold text-[var(--text-color-secondary)]">
-                    <img className="w-[20px] h-[20px]" src="../../public/images/three-dots-vertical.svg" />
-                    <Moment format="hh:mm">{props.call.created_at}</Moment>
-                    <div className="ml-[5px] mr-[8px] p-[5px] text-[10px] font-extrabold rounded border">
-                        <Moment format="A">{props.call.created_at}</Moment>
-                    </div>
-                </div>
-
+            <div className="w-[90%] h-[70px] flex justify-center items-center rounded-xl py-[15px] border">
                 {
-                    props.isArchiveButtonDisplayed &&
-                    <div title="Archive this call" className="mr-[5px] cursor-pointer hover:opacity-70" onClick={archiveCall}>
-                        <img className="w-[25px] h-[25px]" src='../../public/images/archive.svg' />
-                    </div>
-                }
+                    isArchiving ?
+                        <div className="h-full flex text-[14px] font-bold justify-center items-center">
+                            Archiving...
+                        </div>
+                        :
+                        <>
+                            <div className="h-full mx-[20px]">
+                                {
+                                    props.call.direction === "inbound" ?
+                                        <img src="../../public/images/inbound-call.svg" title="Inbound" alt="Inbound" width={20} height={20} />
+                                        :
+                                        <img src="../../public/images/outbound-call.svg" title="Outbound" alt="Outbound" width={20} height={20} />
+                                }
+                            </div>
 
+                            <div className="grow flex flex-col space-y-[5px]">
+                                <div className="flex space-x-[5px]">
+                                    <div className="text-[16px] font-bold text-[var(--text-color-primary)]"
+                                        title={props.call.direction === "inbound" ? 'From' : 'To'}>
+                                        {
+                                            props.call.direction === "inbound" ? props.call.from : props.call.to
+                                        }
+                                    </div>
+
+                                    <div className="w-[16px] h-[16px] flex justify-center items-center bg-red-600 text-white font-bold rounded-full"
+                                        title="Via number">
+                                        {props.call.via}
+                                    </div>
+                                </div>
+
+                                <div className="text-[16px] text-[var(--text-color-secondary)]">
+                                    {
+                                        props.call.call_type === "answered" ?
+                                            props.call.duration + " second" + (props.call.duration == 1 ? "" : "s") :
+                                            props.call.call_type}
+                                </div>
+                            </div>
+
+                            <div className="flex items-center font-bold text-[var(--text-color-secondary)]">
+                                <img className="w-[20px] h-[20px]" src="../../public/images/three-dots-vertical.svg" />
+                                <Moment format="hh:mm">{props.call.created_at}</Moment>
+                                <div className="ml-[5px] mr-[8px] p-[5px] text-[10px] font-extrabold rounded border">
+                                    <Moment format="A">{props.call.created_at}</Moment>
+                                </div>
+                            </div>
+
+                            {
+                                props.isArchiveButtonDisplayed &&
+                                <div title="Archive this call" className="mr-[5px] cursor-pointer hover:opacity-70" onClick={archiveCall}>
+                                    <img className="w-[25px] h-[25px]" src='../../public/images/archive.svg' />
+                                </div>
+                            }
+                        </>
+                }
             </div>
         </div>
     )
